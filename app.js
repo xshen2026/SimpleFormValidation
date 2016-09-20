751b54a09648a67aa606dabb4c0e6c2be151a7d2
XINYI SHEN <XINYI SHEN>
2016-09-20 16:10:18 -0400

    Second Edition, add Email re-enter check

--- app.js
+++ app.js
@@ -126,4 +126,23 @@ app.directive('checkCity', function() {
 		};
 });
 
+app.directive('compareTo', function(){
+	return {
+      require: "ngModel",
+      scope: {
+        otherModelValue: "=compareTo"
+      },
+      link: function(scope, element, attributes, ngModel) {
+
+        ngModel.$validators.compareTo = function(modelValue) {
+          return modelValue == scope.otherModelValue;
+        };
+
+        scope.$watch("otherModelValue", function() {
+          ngModel.$validate();
+        });
+      }
+    };
+});
+
 
